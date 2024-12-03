4ea0210bfc7582e7c25051bf783c30715909c642
LikhithPalya <likhithpalya@gmail.com>
2024-12-03 23:11:53 +0530

    added hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 // import Layout from "../app/layout";
-
+import  HeroSection from "../components/HeroSection"; 
 export default function HomePage() {
   return (
    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      <h1 className="text-2xl font-bold text-center">Welcome to the Home Page</h1>
+      
 
+      
+      <HeroSection />
    </main>
+  
 );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,54 @@
 import type { Config } from "tailwindcss";
-
+import colors from "tailwindcss/colors";  
+import { flattenColorPalette } from "tailwindcss/defaultTheme";
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{ts,tsx}"
   ],
   darkMode: 'class',
   theme: {
     extend: {
+      animation: {
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+      },
+      keyframes: {
+        spotlight: {
+          "0%": {
+            opacity: '0',
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: '1', 
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors,],
 } satisfies Config;
+
+
+function addVariablesForColors({ addBase, theme }: any) {
+  const allColors = theme("colors");
+  const flattenedColors = Object.fromEntries(
+    Object.entries(allColors).flatMap(([color, value]) =>
+      typeof value === "object"
+        ? Object.entries(value).map(([shade, shadeValue]) => [`${color}-${shade}`, shadeValue])
+        : [[color, value]]
+    )
+  );
+
+  addBase({
+    ":root": Object.fromEntries(
+      Object.entries(flattenedColors).map(([key, val]) => [`--${key}`, val])
+    ),
+  });
+}
