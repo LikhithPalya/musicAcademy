f7aec1fab6dcab925c726dac218a2ff139944e78
LikhithPalya <likhithpalya@gmail.com>
2024-12-05 19:48:53 +0530

    card hover effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import FeaturedSection from "@/components/FeaturedSection";
 import  HeroSection from "../components/HeroSection"; 
 import WhyChooseUs from "@/components/WhyChooseUs";
 import TestimonialCard from "@/components/TestimonialCard";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
 export default function HomePage() {
   return (
    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
@@ -13,6 +14,7 @@ export default function HomePage() {
       <FeaturedSection />
       <WhyChooseUs />
       <TestimonialCard />
+      <UpcomingWebinars />
    </main>
   
 );
--- src/components/TestimonialCard.tsx
+++ src/components/TestimonialCard.tsx
@@ -1,7 +1,8 @@
 'use client'
 import React from "react";
 import  { InfiniteMovingCards } from "./ui/infinite-moving-cards";
-
+import {BackgroundGradientAnimation} from "./ui/background-gradient-animation"
+// TODO
 const musicSchoolTestimonials = [
     {
       quote:
@@ -36,6 +37,8 @@ const musicSchoolTestimonials = [
   ];
   function TestimonialCard() {
     return (
+      <>
+      
       <div className="h-[40rem] w-full dark:bg-black dark:bg-grid-white/[0.2] relative flex flex-col items-center justify-center overflow-hidden">
           <h2 className="text-3xl font-bold text-center mb-8 z-10">Hear our Harmony: Voices of success</h2>
           <div className="flex justify-center w-full overflow-hidden px-4 sm:px-6 lg:px-8">
@@ -43,11 +46,12 @@ const musicSchoolTestimonials = [
               <InfiniteMovingCards
                   items={musicSchoolTestimonials}
                   direction="left"
-                  speed="fast"
+                  speed="slow"
         />
               </div>
           </div>
       </div>
+      </>
     )
   }
 
