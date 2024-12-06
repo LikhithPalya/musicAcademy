752e4f53717e9abc77425848e3751d386266be05
LikhithPalya <likhithpalya@gmail.com>
2024-12-06 16:56:08 +0530

    added instructors page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import  HeroSection from "../components/HeroSection";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import TestimonialCard from "@/components/TestimonialCard";
 import UpcomingWebinars from "@/components/UpcomingWebinars";
+import Instructors from "@/components/Instructors";
 export default function HomePage() {
   return (
    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
@@ -15,6 +16,7 @@ export default function HomePage() {
       <WhyChooseUs />
       <TestimonialCard />
       <UpcomingWebinars />
+      <Instructors/>
    </main>
   
 );
