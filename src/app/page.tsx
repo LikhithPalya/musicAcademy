88fd8b40e86cf1e0c1b3716f03207ff9adf8166d
LikhithPalya <likhithpalya@gmail.com>
2024-12-08 07:28:15 +0530

    finished home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import WhyChooseUs from "@/components/WhyChooseUs";
 import TestimonialCard from "@/components/TestimonialCard";
 import UpcomingWebinars from "@/components/UpcomingWebinars";
 import Instructors from "@/components/Instructors";
+import Footer from "@/components/Footer";
 export default function HomePage() {
   return (
    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
@@ -17,6 +18,7 @@ export default function HomePage() {
       <TestimonialCard />
       <UpcomingWebinars />
       <Instructors/>
+      <Footer />
    </main>
   
 );
